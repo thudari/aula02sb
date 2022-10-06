3c7848ca5615b0b36e15df82a94e96e4857aba50
thudari <thudariabib@gmail.com>
2022-10-06 16:52:45 -0300

    Criando as classes do modelo

--- src/main/java/io/github/thudari/aula02sb/Aula02sbApplication.java
+++ src/main/java/io/github/thudari/aula02sb/Aula02sbApplication.java
@@ -1,10 +1,29 @@
 package io.github.thudari.aula02sb;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.thudari.aula02sb.domain.entity.Cliente;
+import io.github.thudari.aula02sb.domain.repository.Clientes;
 
 @SpringBootApplication
 public class Aula02sbApplication {
+	
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		return args -> {
+			clientes.salvar(new Cliente("Dougllas"));
+			clientes.salvar(new Cliente("Outro Cliente"));
+			
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aula02sbApplication.class, args);
