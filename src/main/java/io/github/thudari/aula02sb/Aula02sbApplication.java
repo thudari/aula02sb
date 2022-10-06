846e91fc948ae04837cc862ed345ceb3fe0869af
thudari <thudariabib@gmail.com>
2022-10-06 11:19:44 -0300

    Projeto criado

---
+++ src/main/java/io/github/thudari/aula02sb/Aula02sbApplication.java
@@ -0,0 +1,13 @@
+package io.github.thudari.aula02sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula02sbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula02sbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/thudari/aula02sb/Aula02sbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thudari.aula02sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula02sbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
